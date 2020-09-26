6ab1b80aaa8636bc1020a224e41f247ea2eaa65e
tanhuiqin <61132579+tanhuiqin@users.noreply.github.com>
2020-09-26 18:02:11 +0800

    Add files via upload
    Updated Ultrasonic with mdash and epoch time

---
+++ Ultrasonic/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20200926-093727";
+const char *build_timestamp = "2020-09-26T09:37:27Z";
+const char *build_version = "1.0";
---
+++ Ultrasonic/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20200926-091430/2.17.0-gc31a745";
+const char *mg_build_timestamp = "2020-09-26T09:14:30Z";
+const char *mg_build_version = "2.17.0";
