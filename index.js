bbd35ca9c9a3944450cd546a728dc824a730c447
AugNSo <33392231+AugNSo@users.noreply.github.com>
2020-09-28 22:10:16 +0800

    update index.html & index.js

--- index.js
+++ index.js
@@ -4,16 +4,226 @@ import { COLORS } from '../../constants/colors';
 
 export default (function () {
 
-  const reading1 = document.getElementById('sensor1');
+  const reading1 = document.getElementById('reading1');
+
+  const datetime_now = Math.round(Date.now()/1000);
+  
+  const datetime_1h = datetime_now - 60*60;
+
+  const datetime_1d = datetime_now - 60*60*24;
+
+  var query1 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table16';
+
+  var finalquery1 = query1 + '&min=' + datetime_1h + '&max=' + datetime_now;
 
   if(reading1) {
-    fetch('https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table13&min=1600249454.497599&max=1600249482.498613')
+    fetch(finalquery1)
       .then((response) => {
         response.set
         return response.json();
       })
       .then((data) => {
           reading1.textContent = data.Count;
+      })
+  }
+
+  const reading2 = document.getElementById('reading2');
+  
+
+  var query2 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table16';
+
+  var finalquery2 = query2 + '&min=' + datetime_1d + '&max=' + datetime_now;
+
+  if(reading2) {
+    fetch(finalquery2)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+          reading2.textContent = data.Count;
+      })
+  }
+
+  const reading3 = document.getElementById('reading3');
+
+  var query3 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table16';
+
+  var finalquery3 = query3 + '&min=' + datetime_1h + '&max=' + datetime_now;
+
+  if(reading3) {
+    fetch(finalquery3)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+        var sum = 0;
+        var items = data.Items;
+        items.forEach(element => {
+          sum += parseInt(element.payload.M.duration2.N);
+        });
+        reading3.textContent = Math.round(sum/60);
+      })
+  }
+
+  const reading4 = document.getElementById('reading4');
+
+  var query4 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table16';
+
+  var finalquery4 = query4 + '&min=' + datetime_1d + '&max=' + datetime_now;
+
+  if(reading4) {
+    fetch(finalquery4)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+          var sum = 0;
+          var items = data.Items;
+          items.forEach(element => {
+            sum += parseInt(element.payload.M.duration2.N);
+          });
+          reading4.textContent = Math.round(sum/60);
+      })
+  }
+
+  const reading5 = document.getElementById('reading5');
+  const reading6 = document.getElementById('reading6');
+
+  var query5 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table1';
+
+  var finalquery5 = query5 + '&min=' + datetime_1d + '&max=' + datetime_now;
+
+  if(reading5) {
+    fetch(finalquery5)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+          var sum = 0;
+          var max = 0;
+          var min = 9999999999;
+          var items = data.Items;
+          items.forEach(element => {
+            if(element.payload.M.status2.N == 0){
+              sum += 1;
+              if(element.payload.M.date.N > max){
+                max = element.payload.M.date.N;
+              }
+              if(element.payload.M.date.N < min){
+                min = element.payload.M.date.N;
+              }
+            }
+          });
+          reading5.textContent = sum;
+          reading6.textContent = Math.round((max-min)/sum/60) + " Min " + Math.round((max-min)/sum%60) + " Sec ";
+      })
+  }
+
+  const reading7 = document.getElementById('reading7');
+  const reading8 = document.getElementById('reading8');
+
+  var query7 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table2';
+
+  var finalquery7 = query7 + '&min=' + datetime_1d + '&max=' + datetime_now;
+
+  if(reading7) {
+    fetch(finalquery7)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+          var sum = 0;
+          var max = 0;
+          var min = 9999999999;
+          var items = data.Items;
+          items.forEach(element => {
+            if(element.payload.M.status2.N == 0){
+              sum += 1;
+              if(element.payload.M.date.N > max){
+                max = element.payload.M.date.N;
+              }
+              if(element.payload.M.date.N < min){
+                min = element.payload.M.date.N;
+              }
+            }
+          });
+          reading7.textContent = sum;
+          reading8.textContent = Math.round((max-min)/sum/60) + " Min " + Math.round((max-min)/sum%60) + " Sec ";
+      })
+  }
+
+  const reading9 = document.getElementById('reading9');
+  const reading10 = document.getElementById('reading10');
+
+
+  var query9 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table3';
+
+  var finalquery9 = query9 + '&min=' + datetime_1d + '&max=' + datetime_now;
+
+  if(reading9) {
+    fetch(finalquery9)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+          var sum = 0;
+          var max = 0;
+          var min = 9999999999;
+          var items = data.Items;
+          items.forEach(element => {
+            if(element.payload.M.status2.N == 0){
+              sum += 1;
+              if(element.payload.M.date.N > max){
+                max = element.payload.M.date.N;
+              }
+              if(element.payload.M.date.N < min){
+                min = element.payload.M.date.N;
+              }
+            }
+          });
+          reading9.textContent = sum;
+          reading10.textContent = Math.round((max-min)/sum/60) + " Min " + Math.round((max-min)/sum%60) + " Sec ";
+      })
+  }
+
+  const reading11 = document.getElementById('reading11');
+  const reading12 = document.getElementById('reading12');
+
+
+  var query11 = 'https://z7uwx179ah.execute-api.ap-southeast-1.amazonaws.com/beta/query?table=Table4';
+
+  var finalquery11 = query11 + '&min=' + datetime_1d + '&max=' + datetime_now;
+
+  if(reading11) {
+    fetch(finalquery11)
+      .then((response) => {
+        response.set
+        return response.json();
+      })
+      .then((data) => {
+          var sum = 0;
+          var max = 0;
+          var min = 9999999999;
+          var items = data.Items;
+          items.forEach(element => {
+            if(element.payload.M.status2.N == 0){
+              sum += 1;
+              if(element.payload.M.date.N > max){
+                max = element.payload.M.date.N;
+              }
+              if(element.payload.M.date.N < min){
+                min = element.payload.M.date.N;
+              }
+            }
+          });
+          reading11.textContent = sum;
+          reading12.textContent = Math.round((max-min)/sum/60) + " Min " + Math.round((max-min)/sum%60) + " Sec ";
       })
   }
 
